ff6c84406c5f952909032876713dd7a11331065d
famasboy888 <famasboy88@gmail.com>
2024-04-01 15:56:05 +0800

    Update files

--- day-02/dojo-blog/src/Home.js
+++ day-02/dojo-blog/src/Home.js
@@ -2,19 +2,15 @@ import { useState } from "react";
 
 const Home = () => {
     // let fname = "John Cena"
-    const [name, setName] = useState('John Cena');
-    const [age, setAge] = useState(25);
-
-    const handleClick = () => {
-        setName("John Michael");
-        setAge(age+1);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} yers old!</p>
-            <button onClick={handleClick}>Click Me</button>
+            {blogs.map}
         </div>
     );
 }
