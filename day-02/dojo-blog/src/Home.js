9c45c7ff0ea3b5b1985b5d862a5c42b6ef07b45d
famasboy888 <famasboy88@gmail.com>
2024-04-01 15:50:26 +0800

    Working with states

--- day-02/dojo-blog/src/Home.js
+++ day-02/dojo-blog/src/Home.js
@@ -1,14 +1,20 @@
-const Home = () => {
-    let fname = "John Cena"
+import { useState } from "react";
 
-    const handleClick = (name) => {
-        console.log("Hello " + name)
+const Home = () => {
+    // let fname = "John Cena"
+    const [name, setName] = useState('John Cena');
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName("John Michael");
+        setAge(age+1);
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
-            <button onClick={() => handleClick(fname)}>Click Me</button>
+            <p>{name} is {age} yers old!</p>
+            <button onClick={handleClick}>Click Me</button>
         </div>
     );
 }
