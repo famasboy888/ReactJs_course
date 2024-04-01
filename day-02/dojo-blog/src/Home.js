83f710c495ae84b5346ab06f1ccc2319b6e4a35f
famasboy888 <famasboy88@gmail.com>
2024-04-01 16:28:28 +0800

    Adding branch for props lessons

--- day-02/dojo-blog/src/Home.js
+++ day-02/dojo-blog/src/Home.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     // let fname = "John Cena"
-    const [blogs, setBlogs] = useState([
+    const [blogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
@@ -10,7 +11,8 @@ const Home = () => {
 
     return (
         <div className="home">
-            {blogs.map}
+            <BlogList blogs={blogs} title="All Blogs!"/>
+            <BlogList blogs={blogs} title="Yoshi's Blogs!"/>
         </div>
     );
 }
