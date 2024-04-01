cf3c0f90b8e347ff3746bc15377693e4d345a586
famasboy888 <famasboy88@gmail.com>
2024-04-01 17:30:44 +0800

    Added use Effect

--- day-02/dojo-blog/src/Home.js
+++ day-02/dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -9,15 +9,28 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const [name, setName] = useState("Yoishii")
+
+    const handleChangeName = (new_name) => {
+        setName(new_name);
+    }
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => (blog.id !== id));
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        console.log("from useEffect");
+        console.log(name);
+    }, [name]);
+
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
             <BlogList blogs={blogs.filter((blog) => (blog.author === "mario"))} title="Yoshi's Blogs!" handleDelete={handleDelete} />
+            <button onClick={() => (handleChangeName("wario"))}>Click</button>
+            <p>{name}</p>
         </div>
     );
 }
